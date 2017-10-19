3c03adf21b737840ec9cfde8dbb42d645223b2b5
chadam189 <chadam189@gmail.com>
2017-10-18 22:40:45 -0700

    First pass of FinNdRookSolutions

--- src/solvers.js
+++ src/solvers.js
@@ -34,7 +34,7 @@ window.findNRooksSolution = function(n) {
     // toggle the next spot on the board and check if it's a valid placement
     board.togglePiece(currentRow, currentCol);
     if (board.hasAnyRooksConflicts()) {
-      board.togglePiece(currentRow, currentCol)
+      board.togglePiece(currentRow, currentCol);
     } else {
       rookCounter++;
     }
@@ -74,54 +74,101 @@ window.findNRooksSolution = function(n) {
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
+  if (n === 0) {
+    return 1;
+  }
   var solutionCount = 0; 
-  
   
   // start with new (blank) board of NxN size
   var board = new Board ({n: n});
   // place rook in starting spot
   // initial value will be 0,0 via board.togglePiece(0, 0)
+  var rookRowPositions = {};
+  var rookColPositions = {};
+  var rookPlacements = {};
   var rookCounter = 0;
+  
   board.togglePiece(0, 0);
   rookCounter++;
+  // add starting rook to rookPositions
+  //maybe debug here for string issues
+  rookRowPositions[0] = true;
+  rookColPositions[0] = true;
+  rookPlacements[rookCounter] = [0, 0];
   
+  // initialize currR and currC for the next piece
   var currentRow = 1;
   var currentCol = 1;
   
   //single move loop
-  while (rookCounter < n && currentRow < n) {
-    // toggle the next spot on the board and check if it's a valid placement
-    board.togglePiece(currentRow, currentCol);
-    if (board.hasAnyRooksConflicts()) {
+  while (rookCounter !== 0 && currentRow < n && currentCol < n) {
+    //check if current set up is solution
+    if (rookCounter === n) {
+      solutionCount++;
       board.togglePiece(currentRow, currentCol);
-    } else {
-      rookCounter++;
+      delete rookRowPositions[currentRow];
+      delete rookColPositions[currentCol];
+      delete rookPlacements[rookCounter];
+      rookCounter--;
+      if (currentCol === n - 1) {
+        currentCol = 0;
+        currentRow++;
+      } else {
+        currentCol++;
+      }
     }
-    // increment the next spot on the board
-    if (currentCol === n - 1) {
-      currentCol = 1;
-      currentRow++;
-    } else {
-      currentCol++;
+    
+    //check if current spot is in bounds
+    if (currentRow < n && currentCol < n) {
+      // check against row conflicts with existing pieces
+      while (rookRowPositions.hasOwnProperty(currentRow.toString())) {
+        currentRow++;
+        currentCol = 0;
+      }
+      // check against col conflicts with existing pieces
+      while (rookColPositions.hasOwnProperty(currentCol.toString())) {
+        if (currentCol === n - 1) {
+          currentCol = 0;
+          currentRow++;
+        } else {
+          currentCol++;
+        }
+      }
+      
+      
+      //this is where we check to toggle stuff
+      if (currentRow < n && currentCol < n) {
+        // toggle the next spot on the board; if so, keep it and increment respective counters
+        board.togglePiece(currentRow, currentCol);
+        if (board.hasAnyRooksConflicts()) {
+          board.togglePiece(currentRow, currentCol);
+        } else {
+          rookCounter++;
+          rookRowPositions[currentRow] = true;
+          rookColPositions[currentCol] = true;
+          rookPlacements[rookCounter] = [currentRow, currentCol];
+        }
+      }
+    }
+    
+    //maybe debug here -- check if current spot is off the board
+    if (currentRow === n && rookCounter > 0) {
+      currentRow = rookPlacements[rookCounter][0];
+      currentCol = rookPlacements[rookCounter][1];
+      board.togglePiece(currentRow, currentCol);
+      delete rookRowPositions[currentRow];
+      delete rookColPositions[currentCol];
+      delete rookPlacements[rookCounter];
+      rookCounter--;
+      if (currentCol === n - 1) {
+        currentCol = 0;
+        currentRow++;
+      } else {
+        currentCol++;
+      }
     }
     
   }
-  
-  if (rookCounter === n) {
-    solution = board.rows();
-    
-    return solution;
-  } 
-
-    // rule out rest of (0, 1) to (0, n - 1)
-    // rule out rest of (1, 0) to (n - 1, 0)
-    // while next spot is in bounds AND not breaking one of the two rules above
-    // increment to the next spot (keep row the same, col++... or if at end of row, row++ and col=0)
-      // toggle piece #2
-      // check if board is still valid, if not keep incrementing
-      // if it is, try to add piece #N
-        // check again
-      // if we find a solution, we take a "screencap" by adding current board state (aka this.rows()) to our solution matrix
   
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
